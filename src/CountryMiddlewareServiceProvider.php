b8d59158935eb6b6eac61d1e3cfa45f042962ee5
webazin net <farhadpour4187@gmail.com>
2022-04-18 13:32:12 +0430

    first Commit

---
+++ src/CountryMiddlewareServiceProvider.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Webazin\CountryMiddleware;
+
+use Illuminate\Support\ServiceProvider;
+
+class CountryMiddlewareServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadMigrationsFrom(__DIR__ . '/migrations');
+        $this->publishes([
+            realpath(__DIR__ . '/migrations') => database_path('migrations')
+        ], 'migrations');
+
+        $this->loadRoutesFrom(__DIR__ . '/Routes/routes.php');
+
+        $this->loadViewsFrom(__DIR__ . '/Views', 'CountryMiddleware');
+
+        $this->publishes([
+            __DIR__ . '/Views' => base_path('resource/views/Webazin/CountryMiddleware')
+        ]);
+    }
+}
